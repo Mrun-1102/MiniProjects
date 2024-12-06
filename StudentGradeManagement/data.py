9df1b53ff68502aa604af8ce987e3056abdc3d93
Mrunmai Kashyape <mrunmaikashyape2001@gmail.com>
2024-12-06 20:17:19 +0530

    student Grade Managment

---
+++ StudentGradeManagement/data.py
@@ -0,0 +1,15 @@
+student ={
+    'Paras':100,
+    'Gopal':200
+}
+
+#accessing a element
+print(student['Gopal'])
+
+#Update
+student['Gopal']=300
+print(student)
+
+#delete 
+del student['Paras']
+print(student)
